beb01457971f87d5ffb0d9a4b260fd3dfe286a85
nathanchow <nathannokhangc2027@student.cis.edu.hk>
2024-10-30 10:09:03 +0800

    Complete Lab

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+
+
 public class Main {
 
     /**
@@ -8,8 +10,15 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
+        int[] finalArray = new int [array1.length + array2.length];
+        for (int i = 0; i < array1.length;i++){
+            finalArray[i] = array1[i];
+        }
+        for (int i = 0;i < array2.length;i++){
+            finalArray[i + array1.length] = array2[i];
+        }
 
-        return null;
+        return finalArray;
 
     }
 
@@ -21,9 +30,14 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] finalArray = new int [array1.length + array2.length];
+        int count = 0;
+        for (int i = 0; i < array1.length;i++){
+            finalArray[count] = array1[i];
+            finalArray[count+1] = array2[i];
+            count+=2;
+        }
+        return finalArray;
     }
 
     /**
@@ -34,8 +48,11 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
+        int [] finalArray = new int [array1.length];
+        for (int i = 0;i < array1.length;i++){
+            finalArray[i] = array1[i] * array2[i];
+        }
+        return finalArray;
 
     }
 
@@ -56,15 +73,23 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
-
-        return null;
-
+        int [] newArray = new int [words.length];
+        for (int i = 0; i< words.length;i++){
+            newArray[i] = countCapitalLetters(words[i]);
+        }
+        return newArray;
     }
 
     public static int countCapitalLetters(String word) {
-
-        return 0;
-
+        int count = 0;
+        for (int i = 0; i < word.length();i++){
+            char currentChar = word.charAt(i);
+            int intedChar = (int) currentChar;
+            if (intedChar>= 65 && intedChar<=90){
+                count++;
+            }
+        }
+        return count;
     }
 
 }
